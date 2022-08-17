e4c109d88a7bfa5a9c1be27c40cf5b9e55b47952
yuyu <yuying0215@gmail.com>
2022-08-17 20:13:42 +0800

    update time date

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import requests
 import os
 import random
 
-today = datetime.now()
-print(today)
+today = datetime.now().strftime('%Y-%m-%d')
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -26,14 +25,14 @@ def get_weather():
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  delta = date.today() - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today).days
+  return (next - date.today()).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
